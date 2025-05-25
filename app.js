1b77e0c226e42f434796b4a562647ccfb733399b
yilmazsayin <yilmazzsayinn@gmail.com>
2025-05-25 15:04:09 +0300

    proje vercel düzenlemesi

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const path = require("path");
-
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Routes
 const authRoutes = require("./routes/auth");
-const userRoutes = require('./routes/user');
-const productRoutes = require('./routes/product');
-const ordersRoutes = require('./routes/orders');
+const userRoutes = require("./routes/user");
+const productRoutes = require("./routes/product");
+const ordersRoutes = require("./routes/orders");
 
 // Auth middleware
 const authMiddleware = require("./middleware/authMiddleware");
 
+// MongoDB bağlantısını bir kereye mahsus yapmak için
+let isConnected = false;
+const connectToDatabase = async () => {
+  if (isConnected) return;
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("✅ MongoDB bağlantısı başarılı!");
+  } catch (err) {
+    console.error("❌ MongoDB bağlantısı başarısız:", err.message);
+  }
+};
+
+// Middleware
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: "https://coffely-fe.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", authMiddleware, userRoutes);
 app.use("/api/orders", authMiddleware, ordersRoutes);
@@ -34,16 +48,9 @@ app.use("/api/product", productRoutes);
 app.get("/", (req, res) => {
   res.send("API çalışıyor.");
 });
-const startServer = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB bağlantısı başarılı!");
-    app.listen(PORT, () => {
-      console.log(`⚙️ Server ${PORT} portunda calisiyor.`);
-    });
-  } catch (err) {
-    console.error("❌ MongoDB bağlantısı başarısız.", err.message);
-    process.exit(1);
-  }
+
+// Vercel serverless export
+module.exports = async (req, res) => {
+  await connectToDatabase();
+  return app(req, res);
 };
-startServer();
