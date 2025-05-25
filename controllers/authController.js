db416b378377bb81166cd2bb7c11b9ead21c6804
yilmazsayin <yilmazzsayinn@gmail.com>
2025-05-25 19:55:11 +0300

    cookie token değişikliği

--- controllers/authController.js
+++ controllers/authController.js
@@ -87,7 +87,7 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "None",
       maxAge: 60 * 60 * 1000,
     });
 
